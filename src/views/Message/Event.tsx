2b19d6c8f8bb657902e2f97f7d8fc434a5040a1c
Reshmi Ranjith <rreshmiranjith@gmail.com>
2021-01-27 18:13:20 -0600

    eliminate redudbanancy

--- src/views/Message/Event.tsx
+++ src/views/Message/Event.tsx
@@ -1,25 +1,15 @@
 import React, { useEffect, useState } from "react";
-import styled from "styled-components";
-import { useHistory } from "react-router-dom";
+import Message from "./GenericMessage";
 import { useAuth0 } from "@auth0/auth0-react";
 import { jwt } from "../../api/state";
 import { useRecoilState } from "recoil";
 import axios from "axios";
 
 const Welcome = () => {
-  const history = useHistory();
-  const {
-    isLoading,
-    isAuthenticated,
-    getAccessTokenSilently,
-  } = useAuth0();
+  const { isLoading, isAuthenticated, getAccessTokenSilently } = useAuth0();
   const [token, setToken] = useRecoilState(jwt);
   const [apiComplete, setApiComplete] = useState(false);
   const [event, setEvent] = useState("");
-
-  const start = () => {
-    history.push("/profile");
-  };
 
   useEffect(() => {
     const fn = async () => {
@@ -28,7 +18,7 @@ const Welcome = () => {
       }
     };
     fn();
-  }, [isLoading, isAuthenticated, token]);
+  }, [isLoading, isAuthenticated, token, setToken, getAccessTokenSilently]);
 
   useEffect(() => {
     const config = {
@@ -37,67 +27,25 @@ const Welcome = () => {
       },
     };
     const fn = async () => {
-      const result = await axios
-      .get(
+      const result = await axios.get(
         (process.env.REACT_APP_LOCAL_FUNCTION_URL as string) + "/auth0/checkin",
         config
       );
       setApiComplete(true);
       setEvent(result.data.event);
-    }
+    };
     fn();
-  }, [])
+  }, [getAccessTokenSilently]);
 
   return (
-    <WelcomeComponent>
-      <div className="container">
-        <img
-          className="acm-logo"
-          src="https://www.acmutd.co/brand/General/Assets/Logos/favicon.png"
-          alt="ACM Logo"
-        />
-        <h1 className="text">Thanks for attending!</h1>
-        { apiComplete ? <h3 className="text">Your participation at { event } has been recorded</h3> : null }
-        <button className="retry-button" onClick={start}>
-          Open Profile
-        </button>
-      </div>
-    </WelcomeComponent>
+    <Message
+      title="Thanks for attending!"
+      loading={!apiComplete}
+      desc={`Your participation at ${event} has been recorded`}
+      buttonText="Open Profile"
+      buttonURL="/profile"
+    />
   );
 };
-
-const WelcomeComponent = styled.div`
-  .container {
-    display: flex;
-    background-color: black;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-  }
-  .acm-logo {
-    width: 20%;
-  }
-  .text {
-    color: white;
-    margin: 30px;
-    font-family: ;
-  }
-  .retry-button {
-    outline: none;
-    border: 1px solid transparent;
-    margin: 0px;
-    box-shadow: rgba(0, 0, 0, 0.1) 0px 3px 12px 0px;
-    padding: 8px 18px;
-    min-height: 48px;
-    background-color: rgb(243, 126, 3);
-    color: rgb(255, 255, 255);
-    border-radius: 4px;
-    font-size: 20px;
-  }
-  .retry-button:hover {
-    background-color: rgb(247, 152, 52);
-  }
-`;
 
 export default Welcome;
--- src/views/Message/Loading.tsx
+++ src/views/Message/Loading.tsx
@@ -1,49 +1,8 @@
 import React from "react";
-import styled from "styled-components";
-import ReactLoading from 'react-loading';
+import Message from "./GenericMessage";
 
 const Loading = () => {
-    return <LoadingComponent>
-        <div className="container">
-            <img className="acm-logo" src="https://acmutd.co/brand/General/Assets/Logos/favicon.png" alt="ACM Logo"/>
-            <h1 className="text">Loading</h1>
-            <ReactLoading type={"spinningBubbles"} color={"white"} height={'10%'} width={'10%'} />
-        </div>
-    </LoadingComponent>
-}
-
-const LoadingComponent = styled.div`
-    .container {
-        display: flex;
-        background-color: black;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        height: 100vh;
-    }
-    .acm-logo {
-        width: 20%;
-    }
-    .text {
-        color: white;
-        margin: 30px;
-        font-family: 
-    }
-    .retry-button {
-        outline: none;
-        border: 1px solid transparent;
-        margin: 0px;
-        box-shadow: rgba(0, 0, 0, 0.1) 0px 3px 12px 0px;
-        padding: 8px 18px;
-        min-height: 48px;
-        background-color: rgb(243, 126, 3);
-        color: rgb(255, 255, 255);
-        border-radius: 4px;
-        font-size: 20px;
-    }
-    .retry-button:hover {
-        background-color: rgb(247, 152, 52);
-    }
-`;
+  return <Message title="Loading" loading={true} />;
+};
 
 export default Loading;
