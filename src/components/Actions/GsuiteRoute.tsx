ecce337a3e503ea572fb6b7656f88c7d61c4c267
Stevenmqnguyen <stevenmqnguyen@gmail.com>
2021-06-15 23:22:19 -0500

    Redirect to /gsuite to trigger cloudflare

--- src/components/Actions/GsuiteRoute.tsx
+++ src/components/Actions/GsuiteRoute.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { useRecoilValue } from "recoil";
 import { auth_gsuite } from "../../api/state";
-import { Route } from "react-router-dom";
-import Authorize from "./Authorize";
+import { Route, Redirect } from "react-router-dom";
 
 const GsuiteProtectedRoute = ({ Component, ...rest }: any) => {
   const auth_status = useRecoilValue(auth_gsuite);
-
+  console.log(auth_status);
   return (
     <Route
       {...rest}
       render={() => {
-        return auth_status.is_verified === true ? Component : <Authorize />;
+        return auth_status.is_verified === true ? (
+          Component
+        ) : (
+          <Redirect exact to="/gsuite" />
+        );
       }}
     />
   );
