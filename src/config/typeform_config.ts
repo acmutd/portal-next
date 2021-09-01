8ca627c2b1528be9acd426bd52386d9235e6d71c
Stevenmqnguyen <stevenmqnguyen@gmail.com>
2021-09-01 00:38:59 -0500

    Add env variables for typeforms

--- src/config/typeform_config.ts
+++ src/config/typeform_config.ts
@@ -1,3 +1,5 @@
 export const vanity = process.env.REACT_APP_VANITY_TYPEFORM as string;
 export const pro = process.env.REACT_APP_PROFILE_TYPEFORM as string;
 export const survey = process.env.REACT_APP_PARTICIPATION_TYPEFORM as string;
+export const event = process.env.REACT_APP_EVENT_TYPEFORM as string;
+export const email = process.env.REACT_APP_SENDGRID_TYPEFORM as string;
