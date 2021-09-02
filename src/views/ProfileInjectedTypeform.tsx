0bd4ef3a2ae1b86bbab9a8a3947076466a2933fe
Stevenmqnguyen <stevenmqnguyen@gmail.com>
2021-09-01 19:19:52 -0500

    Update logic for auth, abstract capitalize func

--- src/views/ProfileInjectedTypeform.tsx
+++ src/views/ProfileInjectedTypeform.tsx
@@ -2,36 +2,48 @@ import React from "react";
 import Typeform from "../components/Typeform/typeform";
 import { useRecoilValue } from "recoil";
 import { auth_gsuite } from "../api/state";
+import { unauthorized_form } from "../config/typeform_config";
 
 interface message {
   typeform_id: string;
 }
-const ProfileInjectedTypeform = (props: message) => {
+
+const CapitalizeWord = (word: string) => {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+};
+
+const ProfileInjectedTypeform = ({ typeform_id }: message) => {
   const auth_status = useRecoilValue(auth_gsuite);
-  const email =
-    auth_status.decoded_jwt === undefined
-      ? undefined
-      : auth_status.decoded_jwt["email"];
-  const name = email!.split("@")[0];
-  const firstName =
-    name.split(".")[0].charAt(0).toUpperCase() + name.split(".")[0].slice(1);
-  const lastName =
-    name.split(".")[1].charAt(0).toUpperCase() + name.split(".")[1].slice(1);
-  const typeform_id = props.typeform_id;
+
+  if (auth_status.is_verified) {
+    const email = auth_status.decoded_jwt!["email"];
+    const name = email!.split("@")[0];
+    const firstName = CapitalizeWord(name.split(".")[0]);
+    const lastName = CapitalizeWord(name.split(".")[1]);
+
+    return (
+      <div>
+        <Typeform
+          tfLink={
+            "https://acmutd.typeform.com/to/" +
+            typeform_id +
+            "#email=" +
+            email +
+            "&first_name=" +
+            firstName +
+            "&last_name=" +
+            lastName
+          }
+          style={{ height: "100vh" }}
+        />
+      </div>
+    );
+  }
 
   return (
     <div>
       <Typeform
-        tfLink={
-          "https://acmutd.typeform.com/to/" +
-          typeform_id +
-          "#email=" +
-          email +
-          "&first_name=" +
-          firstName +
-          "&last_name=" +
-          lastName
-        }
+        tfLink={"https://acmutd.typeform.com/to/" + unauthorized_form}
         style={{ height: "100vh" }}
       />
     </div>
