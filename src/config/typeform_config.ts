b89b5bbc090bcf3ca3ab80cae58d3fdf58f20e90
Stevenmqnguyen <stevenmqnguyen@gmail.com>
2021-09-01 19:17:59 -0500

    Add form and unauthorize_form secrets

--- src/config/typeform_config.ts
+++ src/config/typeform_config.ts
@@ -3,3 +3,5 @@ export const pro = process.env.REACT_APP_PROFILE_TYPEFORM as string;
 export const survey = process.env.REACT_APP_PARTICIPATION_TYPEFORM as string;
 export const event = process.env.REACT_APP_EVENT_TYPEFORM as string;
 export const email = process.env.REACT_APP_SENDGRID_TYPEFORM as string;
+export const form = process.env.REACT_APP_FORM_ADDER_TYPEFORM as string
+export const unauthorized_form = process.env.REACT_APP_UNAUTHORIZED_TYPEFORM as string;
