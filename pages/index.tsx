61db1f149d12dd22cd117dbd482399d0e2a674bf
DedsecKnight <mike.nguyen@acmutd.co>
2022-01-20 09:48:47 -0600

    feat(auth): added button to link accounts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   const { data: session } = useSession();
@@ -9,9 +10,19 @@ const Home: NextPage = () => {
     <div>
       <h1 className="text-lg">Signed in as {session.user?.name}</h1>
       <h1 className="text-lg">Email: {session.user?.email}</h1>
-      <button className="p-3 rounded-lg bg-green-400" onClick={() => signOut()}>
-        Sign out
-      </button>
+      <div className="flex gap-x-3">
+        <button
+          className="p-3 rounded-lg bg-green-400"
+          onClick={() => signOut()}
+        >
+          Sign out
+        </button>
+        <Link href="/api/auth/signin">
+          <button className="p-3 rounded-lg bg-green-400">
+            Add another account
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
