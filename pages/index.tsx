2df781ef24e7d923e7a3ae18d4d3e9f02abdd6a3
DedsecKnight <mike.nguyen@acmutd.co>
2022-01-21 01:36:24 -0600

    fix: fixed issue with spacing

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,14 +7,8 @@ export default function HomePage() {
   if (!session) return <div />;
   return (
     <div>
-      <h1 className="text-lg">
-        Signed in as
-        {session.user?.name}
-      </h1>
-      <h1 className="text-lg">
-        Email:
-        {session.user?.email}
-      </h1>
+      <h1 className="text-lg">Signed in as {session.user?.name}</h1>
+      <h1 className="text-lg">Email: {session.user?.email}</h1>
       <div className="flex gap-x-3">
         <button type="button" className="p-3 rounded-lg bg-green-400" onClick={() => signOut()}>
           Sign out
