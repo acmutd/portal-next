f867d0e356eab968f8ae45b0c7e7e936e22d4646
Stevenmqnguyen <stevenmqnguyen@gmail.com>
2022-01-24 01:15:13 -0600

    fix: fix cors header

--- pages/api/graphql/index.ts
+++ pages/api/graphql/index.ts
@@ -14,6 +14,17 @@ const apolloServer = new ApolloServer({
 const startServer = apolloServer.start();
 
 export default cors(async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', 'https://studio.apollographql.com');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Methods, Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Access-Control-Allow-Headers',
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, PATCH, DELETE, OPTIONS, HEAD');
+  if (req.method === 'OPTIONS') {
+    res.end();
+    return false;
+  }
   await startServer;
   await apolloServer.createHandler({
     path: '/api/graphql',
