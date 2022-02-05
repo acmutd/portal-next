07f737be7735a88dd670bb8897088ad135d94b50
Eduardo <edmazuera@gmail.com>
2022-02-04 23:17:53 -0600

    feat(profile): added form validation to profile creation
    using built in react-hook-form validation and regex for validating only correct values get added to
    a profile, also added temporary labels to give a hint to the user what they did wrong while filling
    the form

--- pages/profile/update.tsx
+++ pages/profile/update.tsx
@@ -22,8 +22,16 @@ const CREATE_USER = gql`
 `;
 
 function Onboarding() {
+  /**
+   * Form for first time users of Portal.
+   */
+
   // { errors }
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const [result, setResult] = useState<any>('');
   const { data: session } = useSession();
 
@@ -56,7 +64,7 @@ function Onboarding() {
       >
         Sign out
       </button>
-      <div className="flex flex-col w-[5%]">
+      <div className="flex flex-col w-[20%]">
         <form
           onSubmit={handleSubmit((vals) => {
             setResult(vals);
@@ -64,23 +72,94 @@ function Onboarding() {
             window.location.href = '/';
           })}
         >
-          <input {...register('firstName', { required: true })} placeholder="First name" />
-          <input {...register('lastName', { required: true })} placeholder="Last name" />
-          <input {...register('netid', { required: true })} placeholder="NetId" />
-          <input {...register('major', { required: true })} placeholder="major" />
-          <select {...register('classStanding', { required: true })} placeholder="classification">
+          <input
+            name="firstName"
+            {...register('firstName', {
+              required: 'Enter your First Name.',
+              pattern: {
+                // ok but this also matches symbols????
+                value: /\b[^\d]+\b/gi,
+                // message: "Unless you are Elon Musk"s child, names do not contain numbers."
+                message: 'Names should not contain numbers.',
+              },
+            })}
+            placeholder="First name"
+          />
+          <div className="text-xs text-red-600">{errors.firstName && errors.firstName.message}</div>
+
+          <input
+            name="lastName"
+            {...register('lastName', {
+              required: 'Enter your surname.',
+            })}
+            placeholder="Last name"
+          />
+          <div className="text-xs text-red-600">{errors.lastName && errors.lastName.message}</div>
+
+          <input
+            name="netid"
+            {...register('netid', {
+              required: 'Enter your netID.',
+              pattern: {
+                value: /[\D]{3}[\d]{6}/g,
+                message: 'Please enter a valid netID.',
+              },
+              maxLength: {
+                value: 9,
+                message: 'netID length is too long.',
+              },
+            })}
+            placeholder="NetID"
+          />
+          <div className="text-xs text-red-600">{errors.netid && errors.netid.message}</div>
+
+          <input
+            name="major"
+            {...register('major', {
+              required: 'What is your major?',
+              pattern: /\b[\w]*\b/g,
+            })}
+            placeholder="Major"
+          />
+          <div className="text-xs text-red-600">{errors.major && errors.major.message}</div>
+
+          <select name="classStanding" {...register('classStanding')} placeholder="classification">
             <option value="Freshman">Freshman</option>
             <option value="Sophomore">Sophomore</option>
             <option value="Junior">Junior</option>
             <option value="Senior">Senior</option>
             <option value="Professor">Professor</option>
             <option value="Sponsor">Sponsor</option>
-            <option value="Graduate5">Graduate</option>
+            <option value="Graduate">Graduate</option>
           </select>
-          <input {...register('year', { required: true })} placeholder="Graduation Year" />
-          <input {...register('semester', { required: true })} placeholder="Graduation Semester" />
-          <input id="are_u_utd" type="checkbox" {...register('utdStudent', { required: true })} />
-          <input type="submit" className="cursor-pointer" />
+
+          <input
+            name="year"
+            type="number"
+            {...register('year', {
+              required: 'Enter the year of your graduation.',
+              maxLength: {
+                value: 4,
+                message: 'Please enter a valid year',
+              },
+            })}
+            placeholder="Graduation Year"
+          />
+          <div className="text-xs text-red-600">{errors.year && errors.year.message}</div>
+
+          <input
+            name="semester"
+            {...register('semester', {
+              required: 'What semester are you graduating? (Spring / Summer / Fall)',
+            })}
+            placeholder="Graduation Semester"
+          />
+          <div className="text-xs text-red-600">{errors.semester && errors.semester.message}</div>
+
+          <label htmlFor="studentcheck">Are you a UTD Student? </label>
+          <input id="studentcheck" name="utdStudent" type="checkbox" {...register('utdStudent')} />
+
+          <input type="submit" className="cursor-pointer bg-slate-200 rounded-sm" />
         </form>
       </div>
     </div>
