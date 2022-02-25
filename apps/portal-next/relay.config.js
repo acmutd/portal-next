0066ff850500e729f3556a078408258900311264
Mike Nguyen <mike.nguyen@acmutd.co>
2022-02-25 01:37:51 -0600

    fix: fixed issue with failed build command

--- apps/portal-next/relay.config.js
+++ apps/portal-next/relay.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  src: '.',
+  src: 'apps/portal-next',
   schema: 'apps/portal-next/schema.gql',
   exclude: ['**/node_modules/**', '**/__mocks__/**', '**/__generated__/**'],
   language: 'typescript',
