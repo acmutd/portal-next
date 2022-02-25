626e8e5b404aca97594b3163182f8ebd1d68a2d6
Stevenmqnguyen <stevenmqnguyen@gmail.com>
2022-02-25 12:45:58 -0600

    style: unprettify generated files

--- apps/portal-next/queries/__generated__/resume_GetSignedURlMutation.graphql.ts
+++ apps/portal-next/queries/__generated__/resume_GetSignedURlMutation.graphql.ts
@@ -9,8 +9,8 @@
 // @ts-nocheck
 
 import { ConcreteRequest, Mutation } from 'relay-runtime';
-export type Action = 'DELETE' | 'DOWNLOAD' | 'UPLOAD' | '%future added value';
-export type FileCategory = 'RESUME' | '%future added value';
+export type Action = "DELETE" | "DOWNLOAD" | "UPLOAD" | "%future added value";
+export type FileCategory = "RESUME" | "%future added value";
 export type SignedURLInput = {
   action?: Action | null;
   fileType?: FileCategory | null;
@@ -28,68 +28,68 @@ export type resume_GetSignedURlMutation = {
   response: resume_GetSignedURlMutation$data;
 };
 
-const node: ConcreteRequest = (function () {
-  var v0 = [
+const node: ConcreteRequest = (function(){
+var v0 = [
+  {
+    "defaultValue": null,
+    "kind": "LocalArgument",
+    "name": "options"
+  }
+],
+v1 = [
+  {
+    "alias": null,
+    "args": [
       {
-        defaultValue: null,
-        kind: 'LocalArgument',
-        name: 'options',
-      },
+        "kind": "Variable",
+        "name": "options",
+        "variableName": "options"
+      }
     ],
-    v1 = [
+    "concreteType": "SignedURL",
+    "kind": "LinkedField",
+    "name": "transferFile",
+    "plural": false,
+    "selections": [
       {
-        alias: null,
-        args: [
-          {
-            kind: 'Variable',
-            name: 'options',
-            variableName: 'options',
-          },
-        ],
-        concreteType: 'SignedURL',
-        kind: 'LinkedField',
-        name: 'transferFile',
-        plural: false,
-        selections: [
-          {
-            alias: null,
-            args: null,
-            kind: 'ScalarField',
-            name: 'url',
-            storageKey: null,
-          },
-        ],
-        storageKey: null,
-      },
-    ];
-  return {
-    fragment: {
-      argumentDefinitions: v0 /*: any*/,
-      kind: 'Fragment',
-      metadata: null,
-      name: 'resume_GetSignedURlMutation',
-      selections: v1 /*: any*/,
-      type: 'Mutation',
-      abstractKey: null,
-    },
-    kind: 'Request',
-    operation: {
-      argumentDefinitions: v0 /*: any*/,
-      kind: 'Operation',
-      name: 'resume_GetSignedURlMutation',
-      selections: v1 /*: any*/,
-    },
-    params: {
-      cacheID: '70c40027c5b5adba9a43d55d7fcc6eca',
-      id: null,
-      metadata: {},
-      name: 'resume_GetSignedURlMutation',
-      operationKind: 'mutation',
-      text: 'mutation resume_GetSignedURlMutation(\n  $options: SignedURLInput!\n) {\n  transferFile(options: $options) {\n    url\n  }\n}\n',
-    },
-  };
+        "alias": null,
+        "args": null,
+        "kind": "ScalarField",
+        "name": "url",
+        "storageKey": null
+      }
+    ],
+    "storageKey": null
+  }
+];
+return {
+  "fragment": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "resume_GetSignedURlMutation",
+    "selections": (v1/*: any*/),
+    "type": "Mutation",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Operation",
+    "name": "resume_GetSignedURlMutation",
+    "selections": (v1/*: any*/)
+  },
+  "params": {
+    "cacheID": "70c40027c5b5adba9a43d55d7fcc6eca",
+    "id": null,
+    "metadata": {},
+    "name": "resume_GetSignedURlMutation",
+    "operationKind": "mutation",
+    "text": "mutation resume_GetSignedURlMutation(\n  $options: SignedURLInput!\n) {\n  transferFile(options: $options) {\n    url\n  }\n}\n"
+  }
+};
 })();
 
-(node as any).hash = '5c24bfe040a8726b73e1651493a5cd17';
+(node as any).hash = "5c24bfe040a8726b73e1651493a5cd17";
 
 export default node;
--- apps/portal-next/queries/__generated__/update_CreateProfileMutation.graphql.ts
+++ apps/portal-next/queries/__generated__/update_CreateProfileMutation.graphql.ts
@@ -47,135 +47,135 @@ export type update_CreateProfileMutation = {
   response: update_CreateProfileMutation$data;
 };
 
-const node: ConcreteRequest = (function () {
-  var v0 = [
+const node: ConcreteRequest = (function(){
+var v0 = [
+  {
+    "defaultValue": null,
+    "kind": "LocalArgument",
+    "name": "profile"
+  }
+],
+v1 = [
+  {
+    "alias": null,
+    "args": [
       {
-        defaultValue: null,
-        kind: 'LocalArgument',
-        name: 'profile',
-      },
+        "kind": "Variable",
+        "name": "profile",
+        "variableName": "profile"
+      }
     ],
-    v1 = [
+    "concreteType": "Profile",
+    "kind": "LinkedField",
+    "name": "createProfile",
+    "plural": false,
+    "selections": [
       {
-        alias: null,
-        args: [
-          {
-            kind: 'Variable',
-            name: 'profile',
-            variableName: 'profile',
-          },
-        ],
-        concreteType: 'Profile',
-        kind: 'LinkedField',
-        name: 'createProfile',
-        plural: false,
-        selections: [
-          {
-            alias: null,
-            args: null,
-            kind: 'ScalarField',
-            name: 'email',
-            storageKey: null,
-          },
-          {
-            alias: null,
-            args: null,
-            kind: 'ScalarField',
-            name: 'firstName',
-            storageKey: null,
-          },
-          {
-            alias: null,
-            args: null,
-            kind: 'ScalarField',
-            name: 'lastName',
-            storageKey: null,
-          },
-          {
-            alias: null,
-            args: null,
-            concreteType: 'Graduation',
-            kind: 'LinkedField',
-            name: 'graduation',
-            plural: false,
-            selections: [
-              {
-                alias: null,
-                args: null,
-                kind: 'ScalarField',
-                name: 'semester',
-                storageKey: null,
-              },
-              {
-                alias: null,
-                args: null,
-                kind: 'ScalarField',
-                name: 'year',
-                storageKey: null,
-              },
-            ],
-            storageKey: null,
-          },
-          {
-            alias: null,
-            args: null,
-            kind: 'ScalarField',
-            name: 'classStanding',
-            storageKey: null,
-          },
-          {
-            alias: null,
-            args: null,
-            kind: 'ScalarField',
-            name: '_id',
-            storageKey: null,
-          },
-          {
-            alias: null,
-            args: null,
-            kind: 'ScalarField',
-            name: 'major',
-            storageKey: null,
-          },
-          {
-            alias: null,
-            args: null,
-            kind: 'ScalarField',
-            name: 'membershipStatus',
-            storageKey: null,
-          },
-        ],
-        storageKey: null,
+        "alias": null,
+        "args": null,
+        "kind": "ScalarField",
+        "name": "email",
+        "storageKey": null
       },
-    ];
-  return {
-    fragment: {
-      argumentDefinitions: v0 /*: any*/,
-      kind: 'Fragment',
-      metadata: null,
-      name: 'update_CreateProfileMutation',
-      selections: v1 /*: any*/,
-      type: 'Mutation',
-      abstractKey: null,
-    },
-    kind: 'Request',
-    operation: {
-      argumentDefinitions: v0 /*: any*/,
-      kind: 'Operation',
-      name: 'update_CreateProfileMutation',
-      selections: v1 /*: any*/,
-    },
-    params: {
-      cacheID: '6057b22418e9d928be1b34edcca7fd07',
-      id: null,
-      metadata: {},
-      name: 'update_CreateProfileMutation',
-      operationKind: 'mutation',
-      text: 'mutation update_CreateProfileMutation(\n  $profile: PartialProfile!\n) {\n  createProfile(profile: $profile) {\n    email\n    firstName\n    lastName\n    graduation {\n      semester\n      year\n    }\n    classStanding\n    _id\n    major\n    membershipStatus\n  }\n}\n',
-    },
-  };
+      {
+        "alias": null,
+        "args": null,
+        "kind": "ScalarField",
+        "name": "firstName",
+        "storageKey": null
+      },
+      {
+        "alias": null,
+        "args": null,
+        "kind": "ScalarField",
+        "name": "lastName",
+        "storageKey": null
+      },
+      {
+        "alias": null,
+        "args": null,
+        "concreteType": "Graduation",
+        "kind": "LinkedField",
+        "name": "graduation",
+        "plural": false,
+        "selections": [
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "semester",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "year",
+            "storageKey": null
+          }
+        ],
+        "storageKey": null
+      },
+      {
+        "alias": null,
+        "args": null,
+        "kind": "ScalarField",
+        "name": "classStanding",
+        "storageKey": null
+      },
+      {
+        "alias": null,
+        "args": null,
+        "kind": "ScalarField",
+        "name": "_id",
+        "storageKey": null
+      },
+      {
+        "alias": null,
+        "args": null,
+        "kind": "ScalarField",
+        "name": "major",
+        "storageKey": null
+      },
+      {
+        "alias": null,
+        "args": null,
+        "kind": "ScalarField",
+        "name": "membershipStatus",
+        "storageKey": null
+      }
+    ],
+    "storageKey": null
+  }
+];
+return {
+  "fragment": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "update_CreateProfileMutation",
+    "selections": (v1/*: any*/),
+    "type": "Mutation",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Operation",
+    "name": "update_CreateProfileMutation",
+    "selections": (v1/*: any*/)
+  },
+  "params": {
+    "cacheID": "6057b22418e9d928be1b34edcca7fd07",
+    "id": null,
+    "metadata": {},
+    "name": "update_CreateProfileMutation",
+    "operationKind": "mutation",
+    "text": "mutation update_CreateProfileMutation(\n  $profile: PartialProfile!\n) {\n  createProfile(profile: $profile) {\n    email\n    firstName\n    lastName\n    graduation {\n      semester\n      year\n    }\n    classStanding\n    _id\n    major\n    membershipStatus\n  }\n}\n"
+  }
+};
 })();
 
-(node as any).hash = 'd2fe8ac00a6cee86fdc1aa6a426d3bec';
+(node as any).hash = "d2fe8ac00a6cee86fdc1aa6a426d3bec";
 
 export default node;
