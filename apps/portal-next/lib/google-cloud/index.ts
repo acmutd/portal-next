c16482fcc2a16eabb1d8ab119cdcca672876b1a4
Mike Nguyen <mike.nguyen@acmutd.co>
2022-02-24 23:58:53 -0600

    refactor: added secrets into google cloud storage

--- apps/portal-next/lib/google-cloud/index.ts
+++ apps/portal-next/lib/google-cloud/index.ts
@@ -1,5 +1,11 @@
 import { Storage } from '@google-cloud/storage';
 
-const googleCloudStorage = new Storage();
+const googleCloudStorage = new Storage({
+  projectId: 'acm-core',
+  credentials: {
+    client_email: process.env.GOOGLE_APPLICATION_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_APPLICATION_PRIVATE_KEY,
+  },
+});
 
 export default googleCloudStorage;
