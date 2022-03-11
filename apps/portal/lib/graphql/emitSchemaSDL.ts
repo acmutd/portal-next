d530548218d40d9926e7acd442f5773fb39357d2
Stevenmqnguyen <stevenmqnguyen@gmail.com>
2022-03-11 03:22:37 -0600

    chore: remove unused resolvers

--- apps/portal/lib/graphql/emitSchemaSDL.ts
+++ apps/portal/lib/graphql/emitSchemaSDL.ts
@@ -2,12 +2,14 @@ import 'reflect-metadata';
 import { ObjectId } from 'mongodb';
 import { container } from 'tsyringe';
 import { buildSchema } from 'type-graphql';
-import { resolvers } from './resolvers';
 import ObjectIdScalar from './scalars/ObjectIDScalar';
+import { resolvers } from '@generated/type-graphql';
+import SignedURLResolver from 'lib/graphql/resolvers/SignedURL.resolver';
+import EventCheckinResolver from 'lib/graphql/resolvers/EventCheckin.resolver';
 
 (async () => {
   await buildSchema({
-    resolvers,
+    resolvers: [...resolvers, SignedURLResolver, EventCheckinResolver],
     dateScalarMode: 'isoDate',
     container: {
       get: (someClass) => container.resolve(someClass),
