8031c3968db164591c54d90413fbd56db5e16202
Stevenmqnguyen <stevenmqnguyen@gmail.com>
2022-04-01 16:57:29 -0500

    build: configure TS monorepo friendly configs

--- apps/portal/next.config.js
+++ apps/portal/next.config.js
@@ -1,6 +1,36 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const nextConfig = {};
 
+const isNext12 = (config) => !!config.module.rules.find((rule) => rule.oneOf);
+
+const updateNextGreaterThan12Config = (config) => {
+  const oneOfRule = config.module.rules.find((rule) => rule.oneOf);
+
+  // Next 12 has multiple TS loaders, and we need to update all of them.
+  const tsRules = oneOfRule.oneOf.filter(
+    (rule) => rule.test && rule.test.toString().includes('tsx|ts'),
+  );
+
+  tsRules.forEach((rule) => {
+    // eslint-disable-next-line no-param-reassign
+    rule.include = undefined;
+  });
+
+  return config;
+};
+
+const updateNextLessThan12Config = (config) => {
+  // Next < 12 uses a single Babel loader.
+  const tsRule = config.module.rules.find(
+    (rule) => rule.test && rule.test.toString().includes('tsx|ts'),
+  );
+
+  tsRule.include = undefined;
+  tsRule.exclude = /node_modules/;
+
+  return config;
+};
+
 module.exports = {
   reactStrictMode: true,
   distDir: 'build',
@@ -14,6 +44,10 @@ module.exports = {
       );
     }
     config.experiments = { layers: true, topLevelAwait: true };
-    return config;
+    if (isNext12(config)) {
+      return updateNextGreaterThan12Config(config);
+    }
+
+    return updateNextLessThan12Config(config);
   },
 };
