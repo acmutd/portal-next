9248b2973b248640bb6cdf9e78b8fb45f6fa067c
Mike Nguyen <mike.nguyen@acmutd.co>
2022-04-25 00:10:13 -0500

    fix: fixed eslint issue (hopefullly)

--- services/sendgrid/src/handler.ts
+++ services/sendgrid/src/handler.ts
@@ -1,19 +1,21 @@
-/* eslint @typescript-eslint/no-unsafe-assignment: 0 */
-/* eslint @typescript-eslint/no-unsafe-call: 0 */
-/* eslint @typescript-eslint/no-unsafe-member-access: 0 */
-import { sendEmail, SendEmailConfig } from '@src/send-email';
-import { SQSHandler } from 'aws-lambda';
+import { Context, Callback } from 'aws-lambda';
 import aws from 'aws-sdk';
+import middy from '@middy/core';
+import sqsJsonBodyParser from '@middy/sqs-json-body-parser';
+import { sendEmail, SendEmailConfig } from './send-email';
+import { ValidatedSQSEvent } from './types';
 
-export const main: SQSHandler = async (event, context, callback) => {
+const baseHandler = async (
+  event: ValidatedSQSEvent<SendEmailConfig>,
+  context: Context,
+  callback: Callback
+): Promise<void> => {
   const { Records } = event;
   const stepFunction = new aws.StepFunctions();
   try {
     await Promise.all(
       Records.map(async (record) => {
-        const messageBody = JSON.parse(record.body);
-        const { taskToken } = messageBody;
-        const emailData = messageBody.payload as SendEmailConfig;
+        const { taskToken, payload: emailData } = record.body;
         await sendEmail(emailData);
         await stepFunction
           .sendTaskSuccess({
@@ -21,11 +23,12 @@ export const main: SQSHandler = async (event, context, callback) => {
             output: JSON.stringify({ msg: 'Callback task successful' }),
           })
           .promise();
-        callback(null);
       })
     );
+    callback(null);
   } catch (error) {
-    console.log(error);
     callback(JSON.stringify(error));
   }
 };
+
+export const main = middy(baseHandler).use(sqsJsonBodyParser());
