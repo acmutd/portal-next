a5fb7d3a82d85d0d16556f170abc9c3d16960dc2
Mike Nguyen <mike.nguyen@acmutd.co>
2022-04-28 18:54:40 -0500

    fix: fixed eslint issue and duplicate lambda trigger issue

--- services/sendgrid/src/handler.ts
+++ services/sendgrid/src/handler.ts
@@ -3,34 +3,48 @@ import aws from 'aws-sdk';
 import middy from '@middy/core';
 import sqsJsonBodyParser from '@middy/sqs-json-body-parser';
 import Log from '@dazn/lambda-powertools-logger';
-import { sendEmail, SendEmailConfig } from './send-email';
-import { ValidatedSQSEvent } from './types';
+import { sendEmail, SendEmailConfig } from '@src/send-email';
+import { ValidatedSQSEvent } from '@src/types';
 
 const baseHandler = async (
   event: ValidatedSQSEvent<SendEmailConfig>,
   context: Context,
   callback: Callback
 ): Promise<void> => {
-  const { Records } = event;
-  const stepFunction = new aws.StepFunctions();
   await Promise.all(
-    Records.map(async (record) => {
+    event.Records.map(async (record) => {
+      const stepFunction = new aws.StepFunctions();
       const { taskToken, payload: emailData } = record.body;
+
+      const params = {
+        output: '"Callback task completed successfully."',
+        taskToken,
+      };
+
       try {
+        Log.info(
+          `Sending email from ${emailData.from_name}(${emailData.from}) to ${emailData.to}`,
+          {
+            payload: emailData,
+          }
+        );
         await sendEmail(emailData);
-        return await stepFunction
-          .sendTaskSuccess({
-            taskToken,
-            output: JSON.stringify({ msg: 'Email sent successful' }),
-          })
-          .promise();
+        stepFunction.sendTaskSuccess(params, (err, data) => {
+          if (err) {
+            console.error(err.message);
+            callback(err.message);
+            return;
+          }
+          callback(null);
+        });
       } catch (error) {
-        await stepFunction.sendTaskFailure({ taskToken }).promise();
-        Log.error('Send Email failed', error as Error);
+        stepFunction.sendTaskFailure({ taskToken, error: JSON.stringify(error) }, (err, data) => {
+          console.log(err);
+          callback(err.message);
+        });
       }
     })
   );
-  callback(null);
 };
 
 export const main = middy(baseHandler).use(sqsJsonBodyParser());
