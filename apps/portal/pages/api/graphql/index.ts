6bf1e850caf7999bc25e4fbe6bc39e61ec45d7fb
Stevenmqnguyen <stevenmqnguyen@gmail.com>
2022-06-13 00:51:31 -0400

    refactor: resolve merge conflicts

--- apps/portal/pages/api/graphql/index.ts
+++ apps/portal/pages/api/graphql/index.ts
@@ -1,43 +1,39 @@
 import 'reflect-metadata';
 import { ApolloServer } from 'apollo-server-micro';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
-import { buildSchema } from 'type-graphql';
+import { buildSchemaSync } from 'type-graphql';
 import { container } from 'tsyringe';
 import { ObjectId } from 'mongodb';
 // import { resolvers } from '../../../lib/graphql/resolvers';
 import ObjectIdScalar from '../../../lib/graphql/scalars/ObjectIDScalar';
-
 import { resolvers } from '@generated/type-graphql';
 import SignedURLResolver from 'lib/graphql/resolvers/SignedURL.resolver';
 import EventCheckinResolver from 'lib/graphql/resolvers/EventCheckin.resolver';
 import { getPrismaConnection } from 'lib/prisma/manager';
+
+const schema = buildSchemaSync({
+  resolvers: [...resolvers, SignedURLResolver, EventCheckinResolver],
+  dateScalarMode: 'isoDate',
+  container: {
+    get: (someClass) => container.resolve(someClass),
+  },
+  scalarsMap: [{ type: ObjectId, scalar: ObjectIdScalar }],
+});
+
+const apolloServer = new ApolloServer({
+  schema,
+  introspection: true,
+  plugins: [ApolloServerPluginLandingPageLocalDefault({ footer: false })],
+  context: ({ req }) => ({ req, prisma: getPrismaConnection() }),
+});
+
+const startServer = apolloServer.start();
 
 export default async function handler(req, res) {
   if (req.method === 'OPTIONS') {
     res.end();
     return false;
   }
-
-  const schema = await buildSchema({
-    resolvers: [...resolvers, SignedURLResolver, EventCheckinResolver],
-    dateScalarMode: 'isoDate',
-    container: {
-      get: (someClass) => container.resolve(someClass),
-    },
-    scalarsMap: [{ type: ObjectId, scalar: ObjectIdScalar }],
-  });
-
-  const apolloServer = new ApolloServer({
-    schema,
-    introspection: true,
-    plugins: [ApolloServerPluginLandingPageLocalDefault({ footer: false })],
-    context: ({ req }) => ({ req, prisma: getPrismaConnection() }),
-  });
-
-  const startServer = apolloServer.start();
-
-  // mongoose.set('debug', true);
-  // await mongoose.connect(process.env.MONGODB_URI);
 
   await startServer;
   await apolloServer.createHandler({
