c65a52412d1e2de1e73089e116f2559e81faeab6
Mike Nguyen <mike.nguyen@acmutd.co>
2022-07-01 22:06:02 -0500

    fix: rebase branch

---
+++ packages/acm-ui/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Navbar: React.FC = () => {
+  const msg = 'test EEEEEEE';
+  return <div>{msg}</div>;
+};
+
+export default Navbar;
---
+++ packages/acm-ui/src/components/Template/NAME.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const NAME: React.FC = () => {
+  const msg = 'This is a template component that you can copy in order to create a new component';
+  return <div>{msg}</div>;
+};
+
+export default NAME;
