e57295814e6e3fee08076b5cb594d07c8090b240
Eduardo Mazuera <edmazuera@gmail.com>
2022-07-03 05:11:14 -0500

    feat(portal): implement background
    work on #51

--- apps/portal/pages/_app.tsx
+++ apps/portal/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import Navbar from '../components/Navbar';
 import { createClient, Provider } from 'urql';
+import Background from 'components/Background';
 
 const client = createClient({
   url: '/api/graphql',
@@ -12,6 +13,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider value={client}>
       <SessionProvider session={pageProps.session}>
+        <Background splotches={3} />
         <Navbar>
           <Component {...pageProps} />
         </Navbar>
--- apps/portal/pages/index.tsx
+++ apps/portal/pages/index.tsx
@@ -2,19 +2,21 @@ import { signOut, useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 
-import { ACMButton } from '@acmutd/acm-ui';
+// import { ACMButton } from '@acmutd/acm-ui';
 // run this v when running npm run dev but commit with the above line ^ uncommented (this is temporary till we fix this lol)
-// import { ACMButton } from '@acmutd/acm-ui/src';
+import { ACMButton } from '@acmutd/acm-ui/src';
 
 export default function HomePage() {
   const { data: session } = useSession();
   const router = useRouter();
 
+  let pageTheme: any = 'dark';
+
   if (!session)
     return (
       <>
         <Link href="/auth/signin" passHref>
-          <ACMButton theme="light" gradientColor="8f45c9">
+          <ACMButton theme={pageTheme} gradientColor="8f45c9">
             Sign In
           </ACMButton>
         </Link>
@@ -26,11 +28,11 @@ export default function HomePage() {
       <h1 className="text-lg">Signed in as {session.user?.name}</h1>
       <h1 className="text-lg">Email: {session.user?.email}</h1>
       <div className="flex gap-x-3">
-        <ACMButton onClick={() => signOut()} theme="light">
+        <ACMButton onClick={() => signOut()} theme={pageTheme}>
           Sign out
         </ACMButton>
         <Link href="/auth/signin" passHref>
-          <ACMButton theme="light">Add Another Account</ACMButton>
+          <ACMButton theme={pageTheme}>Add Another Account</ACMButton>
         </Link>
       </div>
     </>
