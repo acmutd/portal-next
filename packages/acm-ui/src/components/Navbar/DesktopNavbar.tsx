3af4a6ecb494c06fe7e4615a6c05170ebbe3714a
Eduardo Mazuera <edmazuera@gmail.com>
2022-07-10 02:18:45 -0500

    feat: start navbar (part 2)
    for some reason these didnt commit

--- packages/acm-ui/src/components/Navbar/DesktopNavbar.tsx
+++ packages/acm-ui/src/components/Navbar/DesktopNavbar.tsx
@@ -2,13 +2,33 @@ import React from 'react';
 import styled from 'styled-components';
 
 interface ACMNavbarPropTypes {
-  children?: React.ReactNode;
+    children?: React.ReactNode;
 }
 
 const Navbar: React.FC = ({ children }: ACMNavbarPropTypes) => {
-  const DesktopDiv = styled.div``;
+    const DesktopDiv = styled.div`
+        display: flex;
+        height: 100%;
+        width: 300px;
+        max-width: 30%;
+        min-width: 12%;
 
-  return <DesktopDiv>{children}</DesktopDiv>;
+        flex-direction: column;
+        justify-content: center;
+        row-gap: 2em;
+        background-color: #212121b3;
+    `;
+
+    return (
+        <DesktopDiv>
+            <div>home</div>
+            <div>events</div>
+            <div>apply</div>
+            <div>profile</div>
+            <div>account</div>
+            <div>resumes</div>
+        </DesktopDiv>
+    );
 };
 
 export default Navbar;
--- packages/acm-ui/src/components/Navbar/MobileNavbar.tsx
+++ packages/acm-ui/src/components/Navbar/MobileNavbar.tsx
@@ -2,13 +2,31 @@ import React from 'react';
 import styled from 'styled-components';
 
 interface ACMNavbarPropTypes {
-  children?: React.ReactNode;
+    children?: React.ReactNode;
 }
 
 const Navbar: React.FC = ({ children }: ACMNavbarPropTypes) => {
-  const MobileDiv = styled.div``;
+    const MobileDiv = styled.div`
+        position: fixed;
+        bottom: 0;
+        height: 100px;
+        width: 100%;
+        background-color: blue;
 
-  return <MobileDiv>{children}</MobileDiv>;
+        display: flex;
+        justify-content: space-evenly;
+    `;
+
+    return (
+        <MobileDiv>
+            <div>home</div>
+            <div>events</div>
+            <div>apply</div>
+            <div>profile</div>
+            <div>account</div>
+            <div>resumes</div>
+        </MobileDiv>
+    );
 };
 
 export default Navbar;
