ccdee713bf5c57a39bec5246412467eb7e7821c9
Eduardo Mazuera <edmazuera@gmail.com>
2022-07-10 02:09:52 -0500

    feat: start navbar

--- apps/portal/components/Skeleton.tsx
+++ apps/portal/components/Skeleton.tsx
@@ -1,11 +1,22 @@
+import { useMediaQuery } from 'react-responsive';
+import Link from 'next/link';
+
 import Background from './Background';
-import Navbar from './Navbar';
+import { ACMDesktopNavbar, ACMMobileNavbar } from '@acmutd/acm-ui/src';
 
 const Skeleton = ({ children }) => {
+  const mobile = useMediaQuery({ maxWidth: 900 });
+
   return (
     <>
       <Background splotches={3} />
-      <Navbar>{children}</Navbar>
+      <div className="h-screen w-screen overflow-hidden">
+        {/* {!mobile &&
+                    <ACMDesktopNavbar />
+                } */}
+        {children}
+        {mobile && <ACMMobileNavbar />}
+      </div>
     </>
   );
 };
--- apps/portal/pages/index.tsx
+++ apps/portal/pages/index.tsx
@@ -2,9 +2,9 @@ import { signOut, useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 
-import { ACMButton } from '@acmutd/acm-ui';
-// run this v when running npm run dev but commit with the above line ^ uncommented (this is temporary till we fix this lol)
-// import { ACMButton } from '@acmutd/acm-ui/src';
+// import { ACMButton } from '@acmutd/acm-ui';
+// commit with above line active and below line uncommented, use below line for testing if you want
+import { ACMButton } from '@acmutd/acm-ui/src';
 
 export default function HomePage() {
   const { data: session } = useSession();
