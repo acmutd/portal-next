83a0103b1938b3b8600e32d828955c4ea6579760
Balstun <ewizard512@gmail.com>
2022-07-24 13:30:47 -0500

    feat(added typeform/embed-react module): the @typeform/embed-react module was added

--- apps/portal/pages/opportunities/index.tsx
+++ apps/portal/pages/opportunities/index.tsx
@@ -4,6 +4,13 @@
  * Route: /opportunities
  *
  */
+import ApplicationPage from '../profile/application';
+
 export default function OpportunitiesPage() {
-  return <div>Opportunities Page</div>;
+  return (
+    <div>
+      <h1>Opportunities Page</h1>
+      <ApplicationPage />
+    </div>
+  );
 }
--- apps/portal/pages/profile/application.tsx
+++ apps/portal/pages/profile/application.tsx
@@ -1,3 +1,16 @@
+import { gql, useMutation, useQuery } from 'urql';
+import { Widget } from '@typeform/embed-react';
+
 export default function ApplicationPage() {
-  return <div />;
+  /*
+  const APPLICATION_QUESTIONS_QUERY = gql`
+    query Query($where: )
+  ` */
+
+  return (
+    <div className="w-screen grid place-items-center gap-2">
+      <h1>Testing Boundaries</h1>
+      <Widget id="rZfmLZaS" width={800} height={800} opacity={80} className="my-form" />
+    </div>
+  );
 }
