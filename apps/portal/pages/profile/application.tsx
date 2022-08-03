91a6785fa69e51485349d00a6d3ce57fd5cd4bd2
Balstun <ewizard512@gmail.com>
2022-08-03 12:11:05 -0500

    feat(added graphql query for active applications): add GraphQL query to retrieve active applications

--- apps/portal/pages/profile/application.tsx
+++ apps/portal/pages/profile/application.tsx
@@ -2,10 +2,31 @@ import { gql, useMutation, useQuery } from 'urql';
 import { Widget } from '@typeform/embed-react';
 
 export default function ApplicationPage() {
-  /*
-  const APPLICATION_QUESTIONS_QUERY = gql`
-    query Query($where: )
-  ` */
+  const ACTIVE_APPLICATIONS_QUERY = gql`
+    query Query($where: TypeformApplicationnWhereInput) {
+      typeformApplicationns(where: $where) {
+        id
+        description
+        typeformId
+        typeformName
+      }
+    }
+  `;
+
+  const [activeTypeformApplicationResult, reexecuteQuery] = useQuery({
+    query: ACTIVE_APPLICATIONS_QUERY,
+    variables: {
+      where: {
+        active: {
+          equals: true,
+        },
+      },
+    },
+  });
+
+  const { data, fetching, error } = activeTypeformApplicationResult;
+  if (fetching) return <p className="text-gray-100">loading...</p>;
+  if (error) return <p className="text-gray-100">whoops... {error.message}</p>;
 
   return (
     <div className="w-screen grid place-items-center gap-2">
