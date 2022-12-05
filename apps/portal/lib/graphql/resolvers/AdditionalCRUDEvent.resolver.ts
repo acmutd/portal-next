c5a2d1369909830af7e958421884f670c16aee2e
Mike Nguyen <mike.nguyen@acmutd.co>
2022-12-04 22:29:28 -0600

    fix: fixed resolver typo

--- apps/portal/lib/graphql/resolvers/AdditionalCRUDEvent.resolver.ts
+++ apps/portal/lib/graphql/resolvers/AdditionalCRUDEvent.resolver.ts
@@ -10,7 +10,7 @@ export default class AdditionalCRUDEventResolver {
   constructor(private eventSerivce: AdditionalCRUDEventService) {}
 
   @Query(() => [Event])
-  async upcomingEvent(): Promise<Event[]> {
+  async upcomingEvents(): Promise<Event[]> {
     return this.eventSerivce.getUpcomingEvent();
   }
 }
