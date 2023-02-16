0703f8542d7d2788973d0a05a0e624109012a10e
abhiamishra <abhiamishra0@gmail.com>
2023-02-16 12:25:28 -0600

    refactor(oldevent.resolver.ts): refactoring code
    Making the null profile check a bit more cleaner

--- apps/portal/lib/graphql/resolvers/OldEvent.resolver.ts
+++ apps/portal/lib/graphql/resolvers/OldEvent.resolver.ts
@@ -26,33 +26,31 @@ export default class OldEventResolver {
   ): Promise<EventCheckin[]> {
     const event_data = await this.firebaseService.returnEventsbyProfile(netId, email);
 
-    if (event_data.length != 0) {
-      const events = await this.eventService.findOldEventID(event_data);
-
-      const profile = await context.prisma.profile.findFirst({
-        where: {
-          netid: netId,
-        },
-      });
-
-      if (profile === null || profile === undefined) {
-        return [];
-      } else {
-        const reservations = await Promise.all(
-          events.map((event) =>
-            this.checkService.checkInEvent(
-              {
-                eventId: event.id,
-                profileId: profile.id,
-              },
-              context,
-            ),
-          ),
-        );
-        return reservations;
-      }
-    } else {
+    if (event_data.length == 0) {
       return [];
     }
+    const events = await this.eventService.findOldEventID(event_data);
+
+    const profile = await context.prisma.profile.findFirst({
+      where: {
+        netid: netId,
+      },
+    });
+
+    if (profile === null || profile === undefined) {
+      return [];
+    }
+    const reservations = await Promise.all(
+      events.map((event) =>
+        this.checkService.checkInEvent(
+          {
+            eventId: event.id,
+            profileId: profile.id,
+          },
+          context,
+        ),
+      ),
+    );
+    return reservations;
   }
 }
