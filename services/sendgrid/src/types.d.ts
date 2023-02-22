daf173a8f9ed11b0dd2d115ab2ba1710218846ed
Mike Nguyen <mike.nguyen@acmutd.co>
2023-02-22 16:12:08 -0600

    removed services folder

--- services/sendgrid/src/types.d.ts
+++
@@ -1,9 +0,0 @@
-import type { SQSEvent, SQSRecord } from 'aws-lambda';
-
-interface SQSBody<T> {
-  payload: T;
-  taskToken: string;
-}
-
-type ValidatedSQSRecord<T> = Omit<SQSRecord, 'body'> & { body: SQSBody<T> };
-type ValidatedSQSEvent<T> = Omit<SQSEvent, 'Records'> & { Records: ValidatedSQSRecord<T>[] };
