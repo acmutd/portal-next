cd77595b7ae770b46ad4c2db008e230eff828036
abhiamishra <abhiamishra0@gmail.com>
2023-03-11 23:46:59 -0600

    committing again for fix

---
+++ apps/documentation/.docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require('C:\\UTD\\Senior Year\\Fall Semester 2022\\ACM Development\\portal-next\\node_modules\\infima\\dist\\css\\default\\default.css'),
+  require('C:\\UTD\\Senior Year\\Fall Semester 2022\\ACM Development\\portal-next\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
+  require('C:\\UTD\\Senior Year\\Fall Semester 2022\\ACM Development\\portal-next\\node_modules\\@docusaurus\\theme-classic\\lib\\admonitions.css'),
+  require('C:\\UTD\\Senior Year\\Fall Semester 2022\\ACM Development\\portal-next\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
+  require('C:\\UTD\\Senior Year\\Fall Semester 2022\\ACM Development\\portal-next\\apps\\documentation\\src\\css\\custom.css'),
+];
---
+++ apps/documentation/.docusaurus/registry.js
@@ -0,0 +1,244 @@
+export default {
+  '01a85c17': [
+    () => import(/* webpackChunkName: '01a85c17' */ '@theme/BlogTagsListPage'),
+    '@theme/BlogTagsListPage',
+    require.resolveWeak('@theme/BlogTagsListPage'),
+  ],
+  '0241b0ea': [
+    () =>
+      import(
+        /* webpackChunkName: '0241b0ea' */ 'C:\\UTD\\Senior Year\\Fall Semester 2022\\ACM Development\\portal-next\\apps\\documentation\\.docusaurus\\docusaurus-plugin-content-pages\\default\\plugin-route-context-module-100.json'
+      ),
+    'C:\\UTD\\Senior Year\\Fall Semester 2022\\ACM Development\\portal-next\\apps\\documentation\\.docusaurus\\docusaurus-plugin-content-pages\\default\\plugin-route-context-module-100.json',
+    require.resolveWeak(
+      'C:\\UTD\\Senior Year\\Fall Semester 2022\\ACM Development\\portal-next\\apps\\documentation\\.docusaurus\\docusaurus-plugin-content-pages\\default\\plugin-route-context-module-100.json'
+    ),
+  ],
+  '0e384e19': [
+    () => import(/* webpackChunkName: '0e384e19' */ '@site/docs/intro.md'),
+    '@site/docs/intro.md',
+    require.resolveWeak('@site/docs/intro.md'),
+  ],
+  17896441: [
+    () => import(/* webpackChunkName: '17896441' */ '@theme/DocItem'),
+    '@theme/DocItem',
+    require.resolveWeak('@theme/DocItem'),
+  ],
+  '1be78505': [
+    () => import(/* webpackChunkName: '1be78505' */ '@theme/DocPage'),
+    '@theme/DocPage',
+    require.resolveWeak('@theme/DocPage'),
+  ],
+  '1df93b7f': [
+    () => import(/* webpackChunkName: '1df93b7f' */ '@site/src/pages/index.tsx'),
+    '@site/src/pages/index.tsx',
+    require.resolveWeak('@site/src/pages/index.tsx'),
+  ],
+  '1f391b9e': [
+    () => import(/* webpackChunkName: '1f391b9e' */ '@theme/MDXPage'),
+    '@theme/MDXPage',
+    require.resolveWeak('@theme/MDXPage'),
+  ],
+  '22f2f7ca': [
+    () =>
+      import(
+        /* webpackChunkName: '22f2f7ca' */ '@site/blog/2022-03-15-welcome/index.md?truncated=true'
+      ),
+    '@site/blog/2022-03-15-welcome/index.md?truncated=true',
+    require.resolveWeak('@site/blog/2022-03-15-welcome/index.md?truncated=true'),
+  ],
+  '37b5286c': [
+    () => import(/* webpackChunkName: '37b5286c' */ '@site/docs/guides/ui components/ACMButton.md'),
+    '@site/docs/guides/ui components/ACMButton.md',
+    require.resolveWeak('@site/docs/guides/ui components/ACMButton.md'),
+  ],
+  '393be207': [
+    () => import(/* webpackChunkName: '393be207' */ '@site/src/pages/markdown-page.md'),
+    '@site/src/pages/markdown-page.md',
+    require.resolveWeak('@site/src/pages/markdown-page.md'),
+  ],
+  '3f0f2f3d': [
+    () => import(/* webpackChunkName: '3f0f2f3d' */ '@site/docs/api-reference/checkin.md'),
+    '@site/docs/api-reference/checkin.md',
+    require.resolveWeak('@site/docs/api-reference/checkin.md'),
+  ],
+  '4d2b1158': [
+    () =>
+      import(
+        /* webpackChunkName: '4d2b1158' */ 'C:\\UTD\\Senior Year\\Fall Semester 2022\\ACM Development\\portal-next\\apps\\documentation\\.docusaurus\\docusaurus-plugin-content-docs\\default\\plugin-route-context-module-100.json'
+      ),
+    'C:\\UTD\\Senior Year\\Fall Semester 2022\\ACM Development\\portal-next\\apps\\documentation\\.docusaurus\\docusaurus-plugin-content-docs\\default\\plugin-route-context-module-100.json',
+    require.resolveWeak(
+      'C:\\UTD\\Senior Year\\Fall Semester 2022\\ACM Development\\portal-next\\apps\\documentation\\.docusaurus\\docusaurus-plugin-content-docs\\default\\plugin-route-context-module-100.json'
+    ),
+  ],
+  '57cdf4e9': [
+    () => import(/* webpackChunkName: '57cdf4e9' */ '@site/docs/guides/monorepo/npm-workspaces.md'),
+    '@site/docs/guides/monorepo/npm-workspaces.md',
+    require.resolveWeak('@site/docs/guides/monorepo/npm-workspaces.md'),
+  ],
+  '5c8abcfb': [
+    () =>
+      import(
+        /* webpackChunkName: '5c8abcfb' */ '@site/docs/getting-started/Installation and Setup.md'
+      ),
+    '@site/docs/getting-started/Installation and Setup.md',
+    require.resolveWeak('@site/docs/getting-started/Installation and Setup.md'),
+  ],
+  '5e9f5e1a': [
+    () => import(/* webpackChunkName: '5e9f5e1a' */ '@generated/docusaurus.config'),
+    '@generated/docusaurus.config',
+    require.resolveWeak('@generated/docusaurus.config'),
+  ],
+  '6875c492': [
+    () => import(/* webpackChunkName: '6875c492' */ '@theme/BlogTagsPostsPage'),
+    '@theme/BlogTagsPostsPage',
+    require.resolveWeak('@theme/BlogTagsPostsPage'),
+  ],
+  69577797: [
+    () => import(/* webpackChunkName: '69577797' */ '~blog/default/blog-tags-general-7ac.json'),
+    '~blog/default/blog-tags-general-7ac.json',
+    require.resolveWeak('~blog/default/blog-tags-general-7ac.json'),
+  ],
+  '814f3328': [
+    () =>
+      import(/* webpackChunkName: '814f3328' */ '~blog/default/blog-post-list-prop-default.json'),
+    '~blog/default/blog-post-list-prop-default.json',
+    require.resolveWeak('~blog/default/blog-post-list-prop-default.json'),
+  ],
+  92688829: [
+    () => import(/* webpackChunkName: '92688829' */ '@site/docs/guides/monorepo/build-tools.md'),
+    '@site/docs/guides/monorepo/build-tools.md',
+    require.resolveWeak('@site/docs/guides/monorepo/build-tools.md'),
+  ],
+  '935f2afb': [
+    () =>
+      import(
+        /* webpackChunkName: '935f2afb' */ '~docs/default/version-current-metadata-prop-751.json'
+      ),
+    '~docs/default/version-current-metadata-prop-751.json',
+    require.resolveWeak('~docs/default/version-current-metadata-prop-751.json'),
+  ],
+  '9e4087bc': [
+    () => import(/* webpackChunkName: '9e4087bc' */ '@theme/BlogArchivePage'),
+    '@theme/BlogArchivePage',
+    require.resolveWeak('@theme/BlogArchivePage'),
+  ],
+  a6aa9e1f: [
+    () => import(/* webpackChunkName: 'a6aa9e1f' */ '@theme/BlogListPage'),
+    '@theme/BlogListPage',
+    require.resolveWeak('@theme/BlogListPage'),
+  ],
+  a7023ddc: [
+    () => import(/* webpackChunkName: 'a7023ddc' */ '~blog/default/blog-tags-tags-4c2.json'),
+    '~blog/default/blog-tags-tags-4c2.json',
+    require.resolveWeak('~blog/default/blog-tags-tags-4c2.json'),
+  ],
+  abd6ce16: [
+    () => import(/* webpackChunkName: 'abd6ce16' */ '@site/docs/api-reference/email.md'),
+    '@site/docs/api-reference/email.md',
+    require.resolveWeak('@site/docs/api-reference/email.md'),
+  ],
+  b1c53161: [
+    () => import(/* webpackChunkName: 'b1c53161' */ '@site/docs/getting-started/portal.md'),
+    '@site/docs/getting-started/portal.md',
+    require.resolveWeak('@site/docs/getting-started/portal.md'),
+  ],
+  b2b675dd: [
+    () => import(/* webpackChunkName: 'b2b675dd' */ '~blog/default/blog-c06.json'),
+    '~blog/default/blog-c06.json',
+    require.resolveWeak('~blog/default/blog-c06.json'),
+  ],
+  b2f554cd: [
+    () => import(/* webpackChunkName: 'b2f554cd' */ '~blog/default/blog-archive-80c.json'),
+    '~blog/default/blog-archive-80c.json',
+    require.resolveWeak('~blog/default/blog-archive-80c.json'),
+  ],
+  b9dfb0e0: [
+    () =>
+      import(
+        /* webpackChunkName: 'b9dfb0e0' */ 'C:\\UTD\\Senior Year\\Fall Semester 2022\\ACM Development\\portal-next\\apps\\documentation\\.docusaurus\\docusaurus-plugin-content-blog\\default\\plugin-route-context-module-100.json'
+      ),
+    'C:\\UTD\\Senior Year\\Fall Semester 2022\\ACM Development\\portal-next\\apps\\documentation\\.docusaurus\\docusaurus-plugin-content-blog\\default\\plugin-route-context-module-100.json',
+    require.resolveWeak(
+      'C:\\UTD\\Senior Year\\Fall Semester 2022\\ACM Development\\portal-next\\apps\\documentation\\.docusaurus\\docusaurus-plugin-content-blog\\default\\plugin-route-context-module-100.json'
+    ),
+  ],
+  ba4ac601: [
+    () => import(/* webpackChunkName: 'ba4ac601' */ '@site/docs/guides/graphql/extending.md'),
+    '@site/docs/guides/graphql/extending.md',
+    require.resolveWeak('@site/docs/guides/graphql/extending.md'),
+  ],
+  c5fad4fc: [
+    () =>
+      import(/* webpackChunkName: 'c5fad4fc' */ '@site/docs/getting-started/serverless-service.md'),
+    '@site/docs/getting-started/serverless-service.md',
+    require.resolveWeak('@site/docs/getting-started/serverless-service.md'),
+  ],
+  ccc49370: [
+    () => import(/* webpackChunkName: 'ccc49370' */ '@theme/BlogPostPage'),
+    '@theme/BlogPostPage',
+    require.resolveWeak('@theme/BlogPostPage'),
+  ],
+  d92a3c43: [
+    () => import(/* webpackChunkName: 'd92a3c43' */ '@site/docs/resources.md'),
+    '@site/docs/resources.md',
+    require.resolveWeak('@site/docs/resources.md'),
+  ],
+  db372864: [
+    () => import(/* webpackChunkName: 'db372864' */ '@site/docs/getting-started/serverless-api.md'),
+    '@site/docs/getting-started/serverless-api.md',
+    require.resolveWeak('@site/docs/getting-started/serverless-api.md'),
+  ],
+  df819268: [
+    () =>
+      import(
+        /* webpackChunkName: 'df819268' */ '@site/docs/principles-architecture/architecture.md'
+      ),
+    '@site/docs/principles-architecture/architecture.md',
+    require.resolveWeak('@site/docs/principles-architecture/architecture.md'),
+  ],
+  df97ccc7: [
+    () =>
+      import(/* webpackChunkName: 'df97ccc7' */ '~blog/default/blog-tags-general-7ac-list.json'),
+    '~blog/default/blog-tags-general-7ac-list.json',
+    require.resolveWeak('~blog/default/blog-tags-general-7ac-list.json'),
+  ],
+  e152d369: [
+    () =>
+      import(
+        /* webpackChunkName: 'e152d369' */ '@site/docs/guides/ui components/create-new-components.md'
+      ),
+    '@site/docs/guides/ui components/create-new-components.md',
+    require.resolveWeak('@site/docs/guides/ui components/create-new-components.md'),
+  ],
+  e279401a: [
+    () =>
+      import(/* webpackChunkName: 'e279401a' */ '@site/docs/guides/graphql/typegraphql-prisma.md'),
+    '@site/docs/guides/graphql/typegraphql-prisma.md',
+    require.resolveWeak('@site/docs/guides/graphql/typegraphql-prisma.md'),
+  ],
+  ee050026: [
+    () => import(/* webpackChunkName: 'ee050026' */ '@site/docs/api-reference/slack.md'),
+    '@site/docs/api-reference/slack.md',
+    require.resolveWeak('@site/docs/api-reference/slack.md'),
+  ],
+  f2281d6a: [
+    () => import(/* webpackChunkName: 'f2281d6a' */ '@site/blog/2022-03-15-welcome/index.md'),
+    '@site/blog/2022-03-15-welcome/index.md',
+    require.resolveWeak('@site/blog/2022-03-15-welcome/index.md'),
+  ],
+  f4d64d56: [
+    () => import(/* webpackChunkName: 'f4d64d56' */ '@site/docs/guides/monorepo/configurations.md'),
+    '@site/docs/guides/monorepo/configurations.md',
+    require.resolveWeak('@site/docs/guides/monorepo/configurations.md'),
+  ],
+  fd0eabb1: [
+    () =>
+      import(
+        /* webpackChunkName: 'fd0eabb1' */ '@site/docs/guides/graphql/dependency-injection.md'
+      ),
+    '@site/docs/guides/graphql/dependency-injection.md',
+    require.resolveWeak('@site/docs/guides/graphql/dependency-injection.md'),
+  ],
+};
