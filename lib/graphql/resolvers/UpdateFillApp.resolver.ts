b5b594bac9585e521a14e7c88fa2fa8d5d9bf1e4
abhiamishra <abhiamishra0@gmail.com>
2023-03-18 20:12:55 -0500

    initialization of updating filled applications

---
+++ lib/graphql/resolvers/UpdateFillApp.resolver.ts
@@ -0,0 +1,42 @@
+import { Event } from '@generated/type-graphql';
+import { injectable } from 'tsyringe';
+import { Arg, Ctx, Int, Mutation, Resolver, UseMiddleware } from 'type-graphql';
+import type { TContext } from '../interfaces/context.interface';
+import FirebaseService from '../services/FirebaseService.service';
+import FilledApp from '../services/FilledApp.service';
+import { InjectSessionMiddleware } from '../middlewares/inject-session';
+import { onlyOfficerAllowed } from '../middlewares/only-officer';
+
+@Resolver(() => Event)
+@injectable()
+export default class UpdateFillAppResolver {
+  constructor(
+    private filledApp: FilledApp
+  ) {}
+
+  @Mutation(() => [])
+  @UseMiddleware(onlyOfficerAllowed, InjectSessionMiddleware)
+  async checkInOldEvent(
+    @Arg('fillAppId', () => String) fillAppId: string,
+    @Arg('status', () => String) status: string,
+    @Arg('score', () => Int) score: number,
+    @Arg('notes', () => String) notes: string,
+    @Arg('interviewLink', () => String) interviewLink: string,
+    @Ctx() context: TContext,
+  ){
+    const userId = context.session!.id
+
+    const profile = await context.prisma.profile.findFirst({
+        where: {
+            userId: userId,
+        },
+    });
+
+    const officerId = profile!.officerId;
+
+    if(officerId != null){
+        const to_update = this.filledApp.getFilledApp(fillAppId)
+        return this.filledApp.updateFilledApp(fillAppId, status, score, notes, interviewLink);
+    }
+  }
+}
---
+++ lib/graphql/services/FilledApp.service.ts
@@ -0,0 +1,32 @@
+import { singleton } from 'tsyringe';
+import { Event, FilledApplication } from '@generated/type-graphql';
+import { getPrismaConnection } from 'lib/prisma/manager';
+
+@singleton()
+export default class FilledApp {
+  async getFilledApp(fillAppId: string): Promise<FilledApplication | null> {
+    const prisma = getPrismaConnection();
+    const eventFilter = {
+      where: {
+        id: fillAppId
+      },
+    };
+    const filled_app = await prisma.filledApplication.findFirst(eventFilter);
+    return filled_app;
+  }
+
+  async updateFilledApp(fillAppId: string, status: string, score:number, notes:string, interviewLink:string) {
+    const prisma = getPrismaConnection();
+    return prisma.filledApplication.update({ 
+      where: {
+        id: fillAppId
+      },
+      update: {
+        status: status,
+        score: score,
+        notes: notes,
+        interviewLink: interviewLink
+      },
+    });
+  }
+}
