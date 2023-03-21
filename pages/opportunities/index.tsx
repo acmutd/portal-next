16ed4d48aa8dc7b1ee3bf692e3b2b698459d09fe
Balstun <ewizard512@gmail.com>
2023-03-21 15:40:18 -0500

    Added temporary changes to main applications page

--- pages/opportunities/index.tsx
+++ pages/opportunities/index.tsx
@@ -1,8 +1,12 @@
 import { PopupButton } from '@typeform/embed-react';
 import Button from 'components/Button';
+import { Button1, Button2 } from 'components/Button1';
 import CircularBlur from 'components/CircularBlur';
 import EmailToast from 'components/EmailToast';
-import ApplicationCard from 'components/typeformApplicationSystem/ApplicationCard';
+import {
+  ApplicationCard,
+  NewApplicationCard,
+} from 'components/typeformApplicationSystem/ApplicationCard';
 import { NextPage } from 'next';
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
@@ -70,6 +74,17 @@ const ApplicationsPage: NextPage = () => {
     <div className="px-16 py-[65px] relative">
       <CircularBlur backgroundColor="rgba(129, 53, 218, 1)" top="20%" left="10%" />
       <CircularBlur backgroundColor="#daa635" bottom="20%" right="15%" />
+      <div className="flex items-start relative mb-[30px] gap-x-4">
+        <h1 className="text-[57px] font-Gilroy text-white font-medium">Hello,</h1>
+        <h1 className="text-[57px] font-Gilroy text-transparent bg-clip-text bg-gradient-to-r from-[#E10087] to-[#4004C0] font-medium">
+          {data!.me.profile.firstName}
+        </h1>
+      </div>
+      <div>
+        <Link href='/opportunities/admin'>
+          <h3 className='text-lg font-Gilroy text-white'>Admin</h3>
+        </Link>
+      </div>
       <header className="flex items-center relative mb-[30px]">
         <h1 className="text-[48px] font-Gilroy text-white font-semibold mx-auto">applications</h1>
 
@@ -79,14 +94,60 @@ const ApplicationsPage: NextPage = () => {
           </Link>
         )}
       </header>
-      <div className="w-full flex flex-wrap gap-[30px]">
+      <div className="w-full flex flex-wrap gap-12 mb-8">
         {data!.typeformApplications.map(
           ({ id, typeformName, description, typeformId, externalResourceUrl, division }) => (
-            <ApplicationCard
+            <NewApplicationCard
               key={id}
               title={typeformName}
               description={description}
-              buttons={[
+              button={
+                <PopupButton
+                  id={typeformId}
+                  hidden={{
+                    email: data!.me.profile.email,
+                    first_name: data!.me.profile.firstName,
+                    last_name: data!.me.profile.lastName,
+                    major: data!.me.profile.major,
+                    net_id: data!.me.profile.netid,
+                    classification: data!.me.profile.classStanding,
+                  }}
+                  className="my-button"
+                >
+                  <Link href='/opportunities/'>
+                    <Button2>View</Button2>
+                  </Link>
+                </PopupButton>
+              }
+              division={division}
+            />
+          ),
+        )}
+      </div>
+      <div className="w-full flex flex-wrap gap-[30px]">
+        {data!.typeformApplications.map(
+          ({ id, typeformName, description, typeformId, externalResourceUrl, division }) => (
+            <NewApplicationCard
+              key={id}
+              title={typeformName}
+              description={description}
+              button={
+                <PopupButton
+                  id={typeformId}
+                  hidden={{
+                    email: data!.me.profile.email,
+                    first_name: data!.me.profile.firstName,
+                    last_name: data!.me.profile.lastName,
+                    major: data!.me.profile.major,
+                    net_id: data!.me.profile.netid,
+                    classification: data!.me.profile.classStanding,
+                  }}
+                  className="my-button"
+                >
+                  <Button1>Apply</Button1>
+                </PopupButton>
+              }
+              /*buttons={[
                 <PopupButton
                   id={typeformId}
                   hidden={{
@@ -109,7 +170,7 @@ const ApplicationsPage: NextPage = () => {
                       </Link>,
                     ]
                   : []),
-              ]}
+              ]}*/
               division={division}
             />
           ),
