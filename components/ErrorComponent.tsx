a492a01da6685a130d3cfb154cff8099dbfb227e
minhhuy24072002 <minhhuy24072002@gmail.com>
2023-04-09 23:02:32 -0500

    add close button to error component

--- components/ErrorComponent.tsx
+++ components/ErrorComponent.tsx
@@ -1,4 +1,5 @@
 import { GraphQLError } from "graphql";
+import { useState } from "react";
 
 interface ErrorComponentProps {
   errorCode: string;
@@ -13,23 +14,35 @@ export default function ErrorComponent({ errorCode, errorMessage}: ErrorComponen
   } else {
     error = errorMessage
   }
+
+  const [showError, setShowError] = useState(true);
+
+  const closeError = () => {
+    setShowError(false);
+  }
+
   return (
-    <div className="bg-red-400 rounded-lg w-full mx-auto p-3 my-4 flex gap-x-3 items-center">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-6 w-6"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        strokeWidth={2}
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-        />
-      </svg>
-      <h1 className="font-medium text-xl">Error: {error}</h1>
-    </div>
+    <>
+      {showError && (
+        <div className="bg-red-400 rounded-lg w-full mx-auto p-3 my-4 flex gap-x-3 items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <h1 className="font-medium text-xl">Error: {error}</h1>
+          <button onClick={closeError}> CLOSE </button>
+        </div>
+      )}
+    </>
   );
 }
