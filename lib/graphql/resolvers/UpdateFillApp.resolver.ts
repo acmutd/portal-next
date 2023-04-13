f12c42f017d600c0cd557e8cc388c1b1ca64083a
abhiamishra <abhiamishra0@gmail.com>
2023-04-12 23:30:48 -0500

    Testing with Mike

--- lib/graphql/resolvers/UpdateFillApp.resolver.ts
+++ lib/graphql/resolvers/UpdateFillApp.resolver.ts
@@ -6,6 +6,7 @@ import FirebaseService from '../services/FirebaseService.service';
 import FilledApp from '../services/FilledApp.service';
 import { InjectSessionMiddleware } from '../middlewares/inject-session';
 import { onlyOfficerAllowed } from '../middlewares/only-officer';
+import { getPrismaConnection } from 'lib/prisma/manager';
 
 @Resolver(() => FilledApplication)
 @injectable()
@@ -13,7 +14,7 @@ export default class UpdateFillAppResolver {
   constructor(private filledApp: FilledApp) {}
 
   @Mutation(() => FilledApplication)
-  @UseMiddleware(onlyOfficerAllowed, InjectSessionMiddleware)
+  @UseMiddleware(InjectSessionMiddleware)
   async updateFilledApp(
     @Arg('fillAppId', () => String) fillAppId: string,
     @Arg('status', () => String) status: string,
@@ -22,16 +23,20 @@ export default class UpdateFillAppResolver {
     @Arg('interviewLink', () => String) interviewLink: string,
     //@Ctx() context: TContext,
   ) {
+    console.log("hi")
+    const prisma = getPrismaConnection();
     //const userId = context.session!.id;
-
-    const profile: Profile | null = await prisma.profile.findFirst({
+    const userId = "635ed2eb1907a687abdc56a2"
+    console.log(userId)
+    const profile = await prisma.profile.findFirst({
       where: {
-        userId: userId,
+        userId: userId
       },
     });
 
     const officer = profile!.officer;
-
+    fillAppId = "641c61babc9b4373e013cac6"
+    console.log("officer")
     if (officer !== null) {
       const to_update = this.filledApp.getFilledApp(fillAppId);
       return this.filledApp.updateFilledApp(fillAppId, status, score, notes, interviewLink);
