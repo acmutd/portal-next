e6f7973ea10ace9baa3e5be01a8c6f65dd1f43fa
abhiamishra <abhiamishra0@gmail.com>
2023-04-19 15:43:12 -0500

    Did a refactor of naming conventions

--- lib/graphql/services/RetrieveAppsByDivision.service.ts
+++ lib/graphql/services/RetrieveAppsByDivision.service.ts
@@ -6,12 +6,12 @@ import { getPrismaConnection } from 'lib/prisma/manager';
 export default class RetrieveAppsByDivision {
   async retrieveApps(divIds: string[]): Promise<Application[]> {
     const prisma = getPrismaConnection();
-    const divisionFilter = {
+    const appFilterbyDiv = {
       where: {
         divisionId: { in: divIds },
       },
     };
-    const apps = await prisma.application.findMany(divisionFilter);
+    const apps = await prisma.application.findMany(appFilterbyDiv);
     return apps;
   }
 }
