648b93ee7a1359b562c4ae0ab34f816f3f156b6f
Solomon Pierce <62899156+spierceVR@users.noreply.github.com>
2023-06-25 21:47:14 -0500

    set gql client url for different environments

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,8 @@ import { GraphQLClient } from 'graphql-request';
 import { QueryClient } from 'react-query';
 import { getSdk } from 'lib/generated/graphql';
 
-const gqlClient = new GraphQLClient('http://localhost:4200/api/graphql');
+const baseUrl = process.env.NODE_ENV == 'production' ? 'https://next.portal.acmutd.co' : 'http://localhost:4200'; 
+const gqlClient = new GraphQLClient(baseUrl + '/api/graphql');
 export const gqlQueries = getSdk(gqlClient);
 
 export const queryClient = new QueryClient({
