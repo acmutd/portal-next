b7d23bf31cb2b5b3b9d37d87818b1a1c1770daee
Solomon Pierce <1nepthys1@gmail.com>
2023-07-25 22:18:51 -0500

    style: match with camelCase

--- lib/graphql/resolvers/ReturnAllOpenApp.resolver.ts
+++ lib/graphql/resolvers/ReturnAllOpenApp.resolver.ts
@@ -16,7 +16,7 @@ export default class ReturnAllOpenAppResolver {
     @Arg('date', () => Date) dateCur: Date,
     @Ctx() context: TContext
   ){
-    const open_apps = await this.returnOpenApp.getOpenApps(dateCur);
-    return open_apps;
+    const openApps = await this.returnOpenApp.getOpenApps(dateCur);
+    return openApps;
   }
 }
