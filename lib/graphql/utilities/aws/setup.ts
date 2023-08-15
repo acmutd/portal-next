14ce51e821ffc4a73892f6070e33113cc923d473
Mike Nguyen <mikenguyen7268@gmail.com>
2023-08-15 12:05:55 -0500

    chore: changed env var name

--- lib/graphql/utilities/aws/setup.ts
+++ lib/graphql/utilities/aws/setup.ts
@@ -3,8 +3,8 @@ import aws from 'aws-sdk';
 export function createStepFunctionInstance() {
   aws.config.update({
     region: 'us-east-1',
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_KEY
+    accessKeyId: process.env.AWS_ACM_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_ACM_SECRET_KEY
   });
 
   return new aws.StepFunctions();
