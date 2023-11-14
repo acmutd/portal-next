5c7f7667b3785812978542a238d1690c2f8e1fe9
Ari S <ari@Aris-MacBook-Pro.local>
2023-11-14 16:36:05 -0600

    added loading animation

--- pages/admin/officer/add.tsx
+++ pages/admin/officer/add.tsx
@@ -10,71 +10,79 @@ import { OfficerStatusContext } from 'components/context/OfficerStatus';
 import { useRouter } from 'next/router';
 import AdminOnlyComponent from 'components/admin/AdminOnly';
 
-
-
 export default function AddOfficerPage() {
-    // Need to get logged in user profile
-    const { status, data: signedInUserData } = useSession({ required: true });
-    const isOfficer = useContext(OfficerStatusContext);
-    const router = useRouter();
-    const { data, error, isLoading } = useQuery(
-        ['addOfficerPage'], 
-        () => gqlQueries.getAddOfficerPageData(),
-        { enabled: status === 'authenticated' && isOfficer }
-    )
-    const [searchQuery, setSearchQuery] = useState<string>("");
-    const [profiles, setProfiles] = useState<GetAddOfficerPageDataQuery["me"]["userProfiles"]>([]);
-    let filteredProfiles = profiles;
-    if (searchQuery !== "") {
-        filteredProfiles = profiles.filter((profile) => 
-            profile.netid === searchQuery 
-            || `${profile.firstName} ${profile.lastName}`.toLowerCase() === searchQuery.toLowerCase()
-            || profile.firstName.toLowerCase() === searchQuery.toLowerCase()
-            || profile.lastName.toLowerCase() === searchQuery.toLowerCase()
-        );
-    } else {
-        filteredProfiles = [];
-    }
+  // Need to get logged in user profile
+  const { status, data: signedInUserData } = useSession({ required: true });
+  const isOfficer = useContext(OfficerStatusContext);
+  const router = useRouter();
+  const { data, error, isLoading } = useQuery(
+    ['addOfficerPage'],
+    () => gqlQueries.getAddOfficerPageData(),
+    { enabled: status === 'authenticated' && isOfficer },
+  );
+  const [searchQuery, setSearchQuery] = useState<string>('');
+  const [profiles, setProfiles] = useState<GetAddOfficerPageDataQuery['me']['userProfiles']>([]);
+  let filteredProfiles = profiles;
+  if (searchQuery !== '') {
+    filteredProfiles = profiles.filter(
+      (profile) =>
+        profile.netid === searchQuery ||
+        `${profile.firstName} ${profile.lastName}`.toLowerCase() === searchQuery.toLowerCase() ||
+        profile.firstName.toLowerCase() === searchQuery.toLowerCase() ||
+        profile.lastName.toLowerCase() === searchQuery.toLowerCase(),
+    );
+  } else {
+    filteredProfiles = [];
+  }
 
-    const debouncedResults = useMemo(() => {
-        return debounce((e) => setSearchQuery(e.target.value), 300);
-    }, []);
+  const debouncedResults = useMemo(() => {
+    return debounce((e) => setSearchQuery(e.target.value), 300);
+  }, []);
 
-    useEffect(() => {
-        if (isLoading) return;
-        if (data) setProfiles(data.me.userProfiles);
-    }, [data, isLoading]);
+  useEffect(() => {
+    if (isLoading) return;
+    if (data) setProfiles(data.me.userProfiles);
+  }, [data, isLoading]);
 
-    useEffect(() => {
-        return () => {
-            debouncedResults.cancel();
-        };
-    });
+  useEffect(() => {
+    return () => {
+      debouncedResults.cancel();
+    };
+  });
 
-    if (isLoading) return <LoadingComponent />;
-    if (!isOfficer) {
-        return <AdminOnlyComponent />;   
-    }
+  if (isLoading) return <LoadingComponent />;
+  if (!isOfficer) {
+    return <AdminOnlyComponent />;
+  }
 
-    return <div className="p-5">
-        <h1 className="text-2xl text-white p-3">Add user to division as officer</h1>
-        {/* Search Box (search by either name or netid) */}
-        <input placeholder='Start off by looking for someone by their name or netid' className="bg-transparent border border-2-gray rounded-2xl w-full lg:w-3/5 text-white" type="text" onChange={debouncedResults} />
+  return (
+    <div className="p-5">
+      <h1 className="text-2xl text-white p-3">Add user to division as officer</h1>
+      {/* Search Box (search by either name or netid) */}
+      <input
+        placeholder="Start off by looking for someone by their name or netid"
+        className="bg-transparent border border-2-gray rounded-2xl w-full lg:w-3/5 text-white"
+        type="text"
+        onChange={debouncedResults}
+      />
 
-
-        {/* Data result (show all data that matches) */}
-        {filteredProfiles.map((profile) => 
-            (<div key={profile.id} className="my-3 w-3/5">
-                <MakeUserOfficerCard 
-                    firstName={profile.firstName}
-                    lastName={profile.lastName}
-                    netid={profile.netid}
-                    divisions={data?.me.profile?.officer?.divisions.map(({ id, deptName }) => ({
-                        id, deptName
-                    })) || []}
-                    profileId={profile.id}
-                />
-            </div>)
-        )}
+      {/* Data result (show all data that matches) */}
+      {filteredProfiles.map((profile) => (
+        <div key={profile.id} className="my-3 w-3/5">
+          <MakeUserOfficerCard
+            firstName={profile.firstName}
+            lastName={profile.lastName}
+            netid={profile.netid}
+            divisions={
+              data?.me.profile?.officer?.divisions.map(({ id, deptName }) => ({
+                id,
+                deptName,
+              })) || []
+            }
+            profileId={profile.id}
+          />
+        </div>
+      ))}
     </div>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ import { useEffect } from 'react';
 import { GetServerSideProps } from 'next';
 import { GraphQLError } from 'graphql';
 import ErrorComponent from 'components/ErrorComponent';
+import { stat } from 'fs';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const { profileVisited } = ctx.req.cookies;
@@ -44,9 +45,38 @@ export default function HomePage({ profileVisited }: { profileVisited: boolean }
   }, [status]);
 
   let pageTheme: any = 'dark';
+  const array = [];
+  for (let i = 0; i < 20; i++) {
+    array.push(
+      <div className="flex items-center justify-between pt-4">
+        <div>
+          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+          <div className="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+        </div>
+        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+      </div>,
+    );
+  }
 
-  if (!session) {
-    return <></>;
+  if (status !== 'authenticated') {
+    return (
+      <>
+        <div
+          role="status"
+          className="w-full h-full p-4 space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700"
+        >
+          <div className="flex items-center justify-between">
+            <div>
+              <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+              <div className="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+            </div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+          </div>
+          {array}
+          <span className="sr-only">Loading...</span>
+        </div>
+      </>
+    );
   }
 
   if (!session)
@@ -66,8 +96,32 @@ export default function HomePage({ profileVisited }: { profileVisited: boolean }
 
   // Fetch data
   // const { data, fetching, error } = profileResult;
-  if (isLoading) return <p className="text-gray-100">loading...</p>;
-  if (error || !data) return <ErrorComponent errorCode={(error as GraphQLError).extensions.code as string} errorMessage={(error as GraphQLError).message}/>;
+  if (isLoading)
+    return (
+      <>
+        <div
+          role="status"
+          className="w-full h-full p-4 space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700"
+        >
+          <div className="flex items-center justify-between">
+            <div>
+              <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+              <div className="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+            </div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+          </div>
+          {array}
+          <span className="sr-only">Loading...</span>
+        </div>
+      </>
+    );
+  if (error || !data)
+    return (
+      <ErrorComponent
+        errorCode={(error as GraphQLError).extensions.code as string}
+        errorMessage={(error as GraphQLError).message}
+      />
+    );
 
   if (!data.profile) {
     router.push('/profile');
