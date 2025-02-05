b9f6b0acad39298913ded129ed69497aa3adff6e
FarhanJamil0001 <farrigalaxy321@gmail.com>
2025-02-04 20:18:05 -0600

    Made some changes to the UI for Org Portal, and also adjusted officer and director managing

--- src/graphql/let orphanedOfficers = [];.js
+++
@@ -1,8 +0,0 @@
-let orphanedOfficers = [];
-db.officers.find().forEach(function(officer) {
-  let profileDoc = db.profiles.findOne({ _id: officer.profileId });
-  if (!profileDoc) {
-    orphanedOfficers.push(officer);
-  }
-});
-printjson(orphanedOfficers);
--- src/graphql/playground-5.mongodb.js
+++
@@ -1,6 +0,0 @@
-// MongoDB Playground
-// Use Ctrl+Space inside a snippet or a string literal to trigger completions.
-
-// The current database to use.
-use("admin");
-
