c04c0e6ef530866736a720bf691fa0d291990806
Jeydin Pham <jeydin@j21.dev>
2025-04-01 13:07:37 -0500

    Fix visual bug on class standing dropdown

--- components/profile/ProfileEditView.tsx
+++ components/profile/ProfileEditView.tsx
@@ -110,7 +110,7 @@ export default function ProfileEditView({ profile, onUpdateFormCompleted, onErro
             <div className="w-full px-3">
               <label className="block text-gray-200 font-semibold mb-2">class standing</label>
               <select
-                className="appearance-none block w-full text-gray-100 rounded py-3 px-4 mb-3 leading-tight focus:outline-none bg-transparent border border-gray-600"
+                className="appearance-none block w-full text-black hover:text-gray-100 rounded py-3 px-4 mb-3 leading-tight focus:outline-none bg-transparent border border-gray-600"
                 {...register('classStanding')}
               >
                 {["freshman", "sophomore", "junior", "senior", "graduate"].map((standing) => (
